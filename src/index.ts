270044ac79b9d16332d2ef26babad4bb060444d3
bulkassi <bulkabulkovich@yandex.ru>
2025-03-08 21:30:55 +0300

    EJS pages and CSS formatting - small adjustments

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,50 @@
-import express, { Request, Response } from 'express';
-const app = express();
-const port = 3000;
+import express, { Request, Response } from 'express'
+const app = express()
+const port = 3000
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// Middlewares
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
-// Главная страница
+// Main page
 app.get('/', (req: Request, res: Response) => {
-    res.render('layout', { body: 'index', title: 'Main' });
-});
+    res.render('layout', { body: 'index', title: 'Main' })
+})
 
-// Страница о магазине
+// About page
 app.get('/about', (req: Request, res: Response) => {
-    res.render('layout', { body: 'about', title: 'About' });
-});
+    res.render('layout', { body: 'about', title: 'About' })
+})
 
-// Страница товара: ручка
+// Pen page
 app.get('/products/pen', (req: Request, res: Response) => {
     const product = {
         name: 'Pen',
         description: 'Blue gel pen',
         price: 10,
-    };
-    res.render('layout', { body: 'product', product, title: product.name });
-});
+    }
+    res.render('layout', { body: 'product', product, title: product.name })
+})
 
-// Страница товара: тетрадь
+// Textbook page
 app.get('/products/notebook', (req: Request, res: Response) => {
     const product = {
         name: 'Textbook',
         description: 'Green textbook, 48 pages',
         price: 15,
-    };
-    res.render('layout', { body: 'product', product, title: product.name });
-});
+    }
+    res.render('layout', { body: 'product', product, title: product.name })
+})
 
-// Динамическая страница
+// Dynamic page
 app.get('/page', (req: Request, res: Response) => {
-    const username = req.query.user || 'Guest';
-    res.render('layout', { body: 'dynamic', username, title: 'Hi, ' });
-});
+    const username = req.query.user || 'Guest'
+    res.render('layout', { body: 'dynamic', username, title: 'Hi, ' })
+})
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-});
+    console.log(`Example app listening on port ${port}`)
+})
