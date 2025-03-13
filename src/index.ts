94cad13698310d2f069e0620f9e4721617eb3c8c
bulkassi <bulkabulkovich@yandex.ru>
2025-03-13 21:51:24 +0300

    Working with Docker and MongoDB; Schemas and endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from 'express'
+import mongoose from "mongoose"
+import User from "./models/User"
 const app = express()
 const port = 3000
 
@@ -6,6 +8,11 @@ const port = 3000
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
+
+mongoose
+    .connect("mongodb://admin:1234@localhost:27017/ivtb-22?authSource=admin")
+    .then(() => console.log("DB connected!"))
+    .catch((err) => console.error(err))
 
 app.set('view engine', 'ejs')
 
@@ -43,6 +50,29 @@ app.get('/products/notebook', (req: Request, res: Response) => {
 app.get('/page', (req: Request, res: Response) => {
     const username = req.query.user || 'Guest'
     res.render('layout', { body: 'dynamic', username, title: 'Hi, ' })
+})
+
+// User creation (endpoint)
+app.post("/users", async (req: Request, res: Response) => {
+    try {
+        console.log(req.body)
+        const user = new User(req.body)
+        await user.save()
+        res.status(201).json(user)
+    } catch (e) {
+        res.status(400).json({message: (e as Error).message})
+    }
+})
+
+// User deletion (endpoint)
+app.delete("/users", async (req: Request, res: Response) => {
+    try {
+        console.log(req.body);
+        const user = await User.deleteOne(req.body);
+        res.status(201).json(user);
+    } catch (e) {
+        res.status(400).json({ message: (e as Error).message })
+    }
 })
 
 app.listen(port, () => {
